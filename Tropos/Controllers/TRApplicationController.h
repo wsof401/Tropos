052dd9e4b2e58173cd5688ad5741ef33080a628d
Jake Craige <james.craige@gmail.com>
2015-05-08 09:07:40 -0700

    Create an ApplicationController and instantiate it in the AppDelegate

---
+++ Tropos/Controllers/TRApplicationController.h
@@ -0,0 +1,16 @@
+//
+//  TRApplicationController.h
+//  Tropos
+//
+//  Created by James Craige on 5/8/15.
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TRWeatherViewController.h"
+
+@interface TRApplicationController : NSObject
+
+@property (nonatomic) TRWeatherViewController *rootViewController;
+
+@end
